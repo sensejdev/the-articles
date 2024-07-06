d0c0efdf7a3ab42576ce5593f749417a069e8ca1
Ramazan Vlilyaev <vlilv.ramazan@gmail.com>
2024-07-06 15:38:20 +0300

    add stylint, i18n plugin for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import i18next from "i18next";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
@@ -12,10 +12,13 @@ export default [
   ...tseslint.configs.recommended,
   pluginReactConfig,
   {
+    plugins: {
+      i18next,
+    },
     rules: {
-          "react/react-in-jsx-scope": "off",
-          "plugin:react/jsx-runtime": "off",
-          "no-unused-vars": "warn"
+      "react/react-in-jsx-scope": "off",
+      "plugin:react/jsx-runtime": "off",
+      "no-unused-vars": "warn",
     },
     settings: {
       "react": {
