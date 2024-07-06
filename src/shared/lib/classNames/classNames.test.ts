f45f1c9032c738da105c39153ce2d47305719c2d
Ramazan Vlilyaev <vlilv.ramazan@gmail.com>
2024-07-06 21:48:00 +0300

    add jest, first tes

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,31 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with only first param", () => {
+    expect(classNames("someClass", {}, [])).toBe("someClass");
+  });
+
+  test("with additional class", () => {
+    expect(classNames("someClass", {}, ["class1", "class2"])).toBe(
+      "someClass class1 class2",
+    );
+  });
+
+  test("with mods", () => {
+    expect(
+      classNames("someClass", { hovered: true, hidden: false }, [
+        "class1",
+        "class2",
+      ]),
+    ).toBe("someClass class1 class2 hovered");
+  });
+
+  test("with undefined mod", () => {
+    expect(
+      classNames("someClass", { hovered: undefined, hidden: true }, [
+        "class1",
+        "class2",
+      ]),
+    ).toBe("someClass class1 class2 hidden");
+  });
+});
